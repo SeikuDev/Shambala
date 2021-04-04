cc956e817fe459bf0965e2b94fdbec0138c0b1ae
SeikuDev <gchhe27@gmail.com>
2021-04-04 01:44:57 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -43,4 +43,15 @@ $(document).ready(function () {
         }
     });
     /*-- End Owl Carousel --*/
+
+    jQuery.event.special.touchstart = {
+        setup: function (_, ns, handle) {
+            this.addEventListener('touchstart', handle, { passive: !ns.includes('noPreventDefault') });
+        }
+    };
+    jQuery.event.special.touchmove = {
+        setup: function (_, ns, handle) {
+            this.addEventListener('touchmove', handle, { passive: !ns.includes('noPreventDefault') });
+        }
+    };
 });
