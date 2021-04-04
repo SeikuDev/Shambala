4ad09e1cb43fd5b35a19a1a10bcfb89cf891faf0
SeikuDev <gchhe27@gmail.com>
2021-04-04 01:48:07 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -44,14 +44,16 @@ $(document).ready(function () {
     });
     /*-- End Owl Carousel --*/
 
-    jQuery.event.special.touchstart = {
-        setup: function (_, ns, handle) {
-            this.addEventListener('touchstart', handle, { passive: !ns.includes('noPreventDefault') });
-        }
-    };
-    jQuery.event.special.touchmove = {
-        setup: function (_, ns, handle) {
-            this.addEventListener('touchmove', handle, { passive: !ns.includes('noPreventDefault') });
-        }
-    };
+
 });
+
+jQuery.event.special.touchstart = {
+    setup: function (_, ns, handle) {
+        this.addEventListener('touchstart', handle, { passive: !ns.includes('noPreventDefault') });
+    }
+};
+jQuery.event.special.touchmove = {
+    setup: function (_, ns, handle) {
+        this.addEventListener('touchmove', handle, { passive: !ns.includes('noPreventDefault') });
+    }
+};
